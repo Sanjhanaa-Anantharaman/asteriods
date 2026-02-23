bd1cf5bb32d87d06afed5f3167a1793196a26410
Sanjhanaa Ananthraman <sanjhanaaananth2006@gmail.com>
2026-02-23 21:27:04 +0530

    add player movement

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -12,15 +12,15 @@ def main():
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
     while 1:
-        dt = clock.tick(60)/1000
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         player.update(dt)
-        log_state()
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
